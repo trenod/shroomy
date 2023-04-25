ed31692486e704e930a32eca247c78627c167ac5
Tobias Kristensen <tkr048@uib.no>
2023-04-26 00:30:39 +0200

    fix

--- app/backend/mushroomApp/settings.py
+++ app/backend/mushroomApp/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ec2-16-170-165-99.eu-north-1.compute.amazonaws.com', '16.170.165.99', "localhost"]
+    'ec2-13-50-15-63.eu-north-1.compute.amazonaws.com', '13.50.15.63', "localhost"]
 
 
 # Application definition
@@ -78,13 +78,8 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'https://www.shroomy.no',
     'https://shroomy.no',
-    'http://ec2-16-16-99-230.eu-north-1.compute.amazonaws.com',
-    'https://ec2-16-16-99-230.eu-north-1.compute.amazonaws.com',
-    'http://ec2-16-170-165-99.eu-north-1.compute.amazonaws.com',
-    'https://ec2-16-170-165-99.eu-north-1.compute.amazonaws.com',
-    'http://16.16.99.230',
-    'http://16.170.165.99',
-    'https://16.16.99.230',
+    'http://ec2-13-50-15-63.eu-north-1.compute.amazonaws.com',
+    'http://13.50.15.63',
     'http://localhost',
     'http://0.0.0.0',
 ]
