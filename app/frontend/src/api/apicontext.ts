02caaa7f47b6cff1547ac1ada4e3975300354027
Tobias Kristensen <tkr048@uib.no>
2023-04-26 21:26:44 +0200

    fix

--- app/frontend/src/api/apicontext.ts
+++ app/frontend/src/api/apicontext.ts
@@ -1,11 +1,8 @@
-export const mushroomServerURL = "http://13.50.15.63:8000";
-//export const mushroomServerURL = "http://localhost:8000";
-export const frontEndServerURL = "http://13.50.15.63:3000";
-//export const frontEndServerURL = "http://localhost:3000";
 
 
-//export const mushroomServerURL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
-//export const frontEndServerURL = process.env.REACT_APP_FRONTEND_BASE_URL || "http://localhost:3000";
+
+export const mushroomServerURL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
+export const frontEndServerURL = process.env.REACT_APP_FRONTEND_BASE_URL || "http://localhost:3000";
 console.log('mushroomServerURL:', mushroomServerURL);
 
 export const mushroomFetch = async ({
